6c48c8d8ab5bf9804dd06784c3d21c0e553c0ed9
Denise James <denisetoo@gmail.com>
2024-10-01 08:40:15 -0500

    added responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import '@aws-amplify/ui-react/styles.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavbarComponent from '../src/components/NavBar/NavBarComponent';
 import './App.css'; // Import your CSS file
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 // Lazy load the components
 const HomeComponent = React.lazy(() => import('./components/Home/HomeComponent'));
@@ -20,7 +21,7 @@ function App() {
     <Authenticator.Provider>
       <Router>
         <NavbarComponent />
-        <div className="content-wrapper"> {/* Add this wrapper */}
+        <div className="container-fluid"> {/* Use Bootstrap container */}
           <Suspense fallback={<div>Loading...</div>}>
             <Routes>
               <Route path="/" element={<HomeComponent />} />
